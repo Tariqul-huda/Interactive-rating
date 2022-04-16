a371f22ff657ab0629593b619c1d1101b7250dfd
Tariqul-huda <tariqulhuda6@gmail.com>
2022-04-16 19:17:56 +0600

    Added style for mobile version

--- Script/script.js
+++ Script/script.js
@@ -5,31 +5,41 @@ const cardRating = document.getElementById("rating");
 const thank = document.getElementById("thank");
 const rate = document.getElementsByClassName("card__selection__paragraph__box");
 let value = 0;
-list.forEach(item=>{
-    item.addEventListener("click",event=>{
-        value=parseInt(event.target.textContent);
-        item.classList.toggle("card--circle--orange")
-        for(let count=0;count<list.length;count++){
-            if(parseInt(list[count].textContent)!==value){
-                list[count].classList.remove("card--circle--orange")
-            }
+function isMobile(){
+    if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i) 
+    || navigator.userAgent.match(/iPad/i) 
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function addCircleOrange(item, list){
+     value = parseInt(event.target.textContent);
+    item.classList.toggle("card--circle--orange");
+    for(let count =0;count<list.length;count++){
+        if(parseInt(list[count].textContent)!== value){
+            list[count].classList.remove("card--circle--orange");
         }
-    });
+    }
+}
+if(isMobile()){
+    list.forEach(item=>{
+        item.addEventListener("touchend",addCircleOrange.bind(this,item,list))
+    })
+
+}
+list.forEach(item=>{
+    item.addEventListener("click",addCircleOrange.bind(this,item,list))
 })
+
 submit.addEventListener("click",()=>{
     cardRating.classList.toggle("hidden");
     thank.classList.toggle('hidden')
     rate[0].innerHTML=`<p class="card__selection__paragraph">You selected ${value} out of 5</p>`
 
 })
-list.forEach(item=>{
-    item.addEventListener("touchend",event=>{
-        value=parseInt(event.target.textContent);
-        item.classList.toggle("card--circle--orange")
-        for(let count=0;count<list.length;count++){
-            if(parseInt(list[count].textContent)!==value){
-                list[count].classList.remove("card--circle--orange")
-            }
-        }
-    })
-});
